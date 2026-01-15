6136a67df62087dd879ff7588e4598a5f1cdc0d1
Ankita Biswas <ankita@Ankitas-MacBook-Air-2.local>
2026-01-15 09:46:37 +0530

    backend completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Navigate, Route,Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import NotificationsPage from './pages/NotificationsPage.jsx'
+import CallPage from './pages/CallPage.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+import OnboardingPage from './pages/OnboardingPage.jsx'
+import toast, { Toaster } from 'react-hot-toast'
+import { useEffect,useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { axiosInstance } from './lib/axios.js'
+
+const App = () => {
+
+
+  const {data:authData,isLoading,error}=useQuery({queryKey:["authUser"],queryFn:async()=>{
+    const res=await axiosInstance.get("/auth/me");
+   
+   return res.data;
+
+},
+retry:false
+})
+const authUser=authData?.user
+  
+
+  return (
+    <div className= "h-screen" data-theme="night">
+      <button onClick={()=>toast.error("hrllo orld")}></button>
+      <Routes>
+        <Route path="/" element={authUser? <HomePage/> : <Navigate to="/login"/>}/>
+        <Route path="/signup" element={ !authUser ? <SignUpPage/> : <Navigate to="/"/>}/>
+        <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/"/>}/>
+        <Route path="/notifications" element={authUser? <NotificationsPage/> : <Navigate to="/login"/>}/>
+        <Route path="/call" element={authUser? <CallPage/> : <Navigate to="/login"/>}/>
+        <Route path="/chat" element={authUser? <ChatPage/> : <Navigate to="/login"/>}/>
+        <Route path="/onboarding" element={authUser? <OnboardingPage/> : <Navigate to="/login"/>}/>
+        
+
+
+      </Routes>
+      <Toaster/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,158 @@
+import { useMutation,useQueryClient } from '@tanstack/react-query';
+import {  ShipWheelIcon } from 'lucide-react';
+import React from 'react'
+import {useState} from "react";
+// import { Link } from 'react-router';
+import { axiosInstance } from '../lib/axios';
+import { Link } from 'react-router-dom'
+
+
+
+
+
+const SignUpPage = () => {
+
+  const [signupData, setSignupData] = useState({
+    fullName:"",
+    email:"",
+    password:"",
+  })
+  const queryClient = useQueryClient();
+  const {mutate,isPending, error} = useMutation({
+    mutationFn: async ()=>{
+      // signup api call
+      const response = await axiosInstance.post("/auth/signup", signupData);
+      return response.data;
+
+    },
+    onSuccess: ()=>queryClient.invalidateQueries({queryKey:["authUser"]}),
+
+
+    
+  })
+  const handleSignup=(e)=>{
+ 
+    e.preventDefault()
+    mutate();
+  }
+  return (
+    <div className='h-screen flex items-center justify-center p-4 sm:p-6 md:p-8' data-theme="forest">
+      <div className='border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden'>
+
+      <div className='w-full lg:w-1/2 p-4  sm:p-8 flex flex-col '>
+
+      <div className='mb-4 flex items-center justify-start gap-2'>
+        <ShipWheelIcon className="size-9 text-primary"/>
+        <span className='text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider'>
+        Streamzy
+        </span>
+      </div>
+
+      <div className='w-full'>
+        <form onSubmit={handleSignup}>
+          <div className='space-y-4'>
+            <div>
+              <h2 className='text-xl font-semibold'>Create an Account</h2>
+                <p>
+                  Join Streamzy and start your language learning adventure today!
+                </p>
+                
+            </div>
+            <div className='space-y-2'>
+              {/* full name */}
+              <div className='form-control w-full'>
+                <label className='label'>
+                <span className='label-text'>
+                  Full Name</span>
+                </label>
+                <input type="text" placeholder='John Doe'  className='input input-bordered w-full'
+                value={signupData.fullName}
+                onChange={(e)=> setSignupData({...signupData, fullName:e.target.value})}
+                required/>           
+
+              </div>
+              {/* email */}
+              <div className='form-control w-full'>
+                <label className='label'>
+                <span className='label-text'>Email</span>
+                </label>
+                <input type="email" placeholder='johndoe@gmail.com'  className='input input-bordered w-full'
+                value={signupData.email}
+                onChange={(e)=> setSignupData({...signupData, email:e.target.value})}
+                required/>           
+
+              </div>
+              {/* password */}
+
+              <div className='form-control w-full'>
+                <label className='label'>
+                <span className='label-text'>
+                 password</span>
+                </label>
+                <input type="password" placeholder='*******'  className='input input-bordered w-full'
+                value={signupData.password}
+                onChange={(e)=> setSignupData({...signupData, password:e.target.value})}
+                required/>   
+                <p className='text-xs opacity-70 mt-1'>
+                  Password must be atleast 6 characters long</p>        
+
+              </div>
+
+              <div className='form-control'>
+                <label className='label cursor-pointer justify-start gap-2'>
+                  <input type="checkbox" className='checkbox checkbox-sm' required/>
+                  <span className='text-xs leading-tight'>
+                    I agree to the {" "}
+                    <span className='text-primary hover:underline'>terms of service</span>
+                    <span className='text-primary hover:underline'>privacy policy</span>
+                  </span>
+                </label>
+              </div>
+            </div>
+            <button className='btn btn-primary w-full' type="submit">{isPending?"Signing up...":"Create Account"}</button>
+
+            <div className='text-center mt-4'>
+              <p className='text-sm'>
+                Already have an account?{" "}
+                <Link to="/login" className="text-primary hover:underline">
+                Sign in
+                </Link>
+
+              </p>
+            </div>
+
+
+          </div>
+        </form>
+
+      </div>
+
+
+      </div>
+
+
+
+
+      <div className="hidden lg:flex w-full lg:w-1/2 bg-primary/10 items-center justify-center">
+          <div className="max-w-md p-8">
+            {/* Illustration */}
+            <div className="relative aspect-square max-w-sm mx-auto">
+              <img src="/i.png" alt="Language connection illustration" className="w-full h-full" />
+            </div>
+
+            <div className="text-center space-y-3 mt-6">
+              <h2 className="text-xl font-semibold">Connect with language partners worldwide</h2>
+              <p className="opacity-70">
+                Practice conversations, make friends, and improve your language skills together
+              </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default SignUpPage
